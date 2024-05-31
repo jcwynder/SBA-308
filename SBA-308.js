704f4a780d9f315f98cd88129b23c6d33ddc6370
Jermal Wynder <jermalwynder@gmail.com>
2024-05-31 04:35:05 -0400

    assignments creation

--- SBA-308.js
+++ SBA-308.js
@@ -1,5 +1,45 @@
 // Information for course
 const CourseInfo = {
   id: 308,
-  name: "Introduction to JavaScript",
+  name: `Introduction to JavaScript`,
+};
+
+// Assignmetns for course
+const AssignmentGroup = {
+  id: 803,
+  name: `Fundamentals of JavaScript`,
+  course_id: 308,
+  group_weight: 10,
+  assignments: [
+    {
+      id: 1,
+      name: `Data Manipulation with JavaScript`,
+      due_at: `2024-05-22`,
+      points_possible: 100,
+    },
+    {
+      id: 2,
+      name: `Control Flow`,
+      due_at: `2024-05-23`,
+      points_possible: 50,
+    },
+    {
+      id: 3,
+      name: `Practical Loops`,
+      due_at: `2024-05-24`,
+      points_possible: 100,
+    },
+    {
+      id: 4,
+      name: `Working with Data Collections`,
+      due_at: `2024-05-27`,
+      points_possible: 200,
+    },
+    {
+      id: 5,
+      name: `Creating Reusable Functions`,
+      due_at: `2024-05-29`,
+      points_possible: 100,
+    },
+  ],
 };
